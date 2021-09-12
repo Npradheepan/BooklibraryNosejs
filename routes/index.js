0ce108885f870f3e0768ed389fd2c01405437d5f
pradh <pradh@DESKTOP-765MFUR>
2021-09-12 21:50:43 +0200

    Initial Author Routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res)=>{
-    res.send("Hallo thie page from router")
+    res.render('index')
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,28 +1,35 @@
-const express = require("express")
-const app = express();
-const expressLayouts = require("express-ejs-layouts")
-const indexRouter = require('./routes/index')
-const mongoose = require('mongoose')
-require('dotenv/config')
+    const express = require("express")
+    const app = express();
+    const expressLayouts = require("express-ejs-layouts")
+    const mongoose = require('mongoose')
+    const bodyParser = require ('body-parser')
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname +'/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
+    //Router Requirements / module Import
+    const indexRouter = require('./routes/index')
+    const authorRouter = require('./routes/authors')
 
-mongoose.set('useNewUrlParser', true)
-mongoose.set('useUnifiedTopology', true)
+    require('dotenv/config')
 
-mongoose.connect(process.env.DATABASE_URL, (err)=>{
-     if(err){
-        console.log('Mongo DB Connected Error!')
-    }else {
-        console.log('Mongo DB Connection Succesfully!');
-    }
-})
+    //Templet Engine
+    app.set('view engine', 'ejs')
+    app.set('views', __dirname + '/views')
+    app.set('layout', 'layouts/layout')
+    app.use(expressLayouts)
+    app.use(express.static('public'))
+    app.use(bodyParser.urlencoded ({limit:'10mb', extended:false }))
 
-
-app.get('/',indexRouter)
-
-app.listen(process.env.PORT || 3000)
+    mongoose.set('useNewUrlParser', true)
+    mongoose.set('useUnifiedTopology', true)
+    //Mongo DB Connection Reference
+    mongoose.connect(process.env.DATABASE_URL, (err)=>{
+        if(err){
+            console.log('Mongo DB Connected Error!')
+        }else {
+            console.log('Mongo DB Connection Succesfully!');
+        }
+    })
+    //Use The Router
+    app.use('/', indexRouter)
+    app.use('/authors', authorRouter)
+    //Connection of port
+    app.listen(process.env.PORT || 3000)
