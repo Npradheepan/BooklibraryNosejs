30dd19432b55179c044ddf63cdf72ec3766a162d
pradh <pradh@DESKTOP-765MFUR>
2021-12-27 11:54:14 +0100

    Initial Setup

--- server.js
+++ server.js
@@ -14,7 +14,7 @@
     //Templet Engine
     app.set('view engine', 'ejs')
     app.set('views', __dirname + '/views')
-    app.set('layout', 'layouts/layout')
+    app.set('layout', 'layouts/layout') 
     app.use(expressLayouts)
     app.use(express.static('public'))
     app.use(bodyParser.urlencoded ({limit:'10mb', extended:false }))
